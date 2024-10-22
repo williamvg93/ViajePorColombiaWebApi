f61af662bbb0a1581c6ef0fc48c0dcbdffd03938
williamvg93 <sonico_5071@hotmail.com>
2024-10-22 12:14:17 -0500

    feat: :sparkles: The configuration for the DB connection was added and the first migration was created
    In appsettings the connection string to the Mysql DB was added, in program the configuration for the connection to the database was added, the migrations will be added in the Migrations folder which is located in the Data folder

---
+++ Api/Persistence/data/migrations/20241022170414_FirstMigration.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.data.migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "journey",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Origin = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Destination = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Price = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "trasport",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FlightCarries = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FlightNumber = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "flight",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    fk_IdTrasport = table.Column<int>(type: "int", nullable: false),
+                    Origin = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Destination = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Price = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_flight_trasport_fk_IdTrasport",
+                        column: x => x.fk_IdTrasport,
+                        principalTable: "trasport",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "JourneyFlight",
+                columns: table => new
+                {
+                    FlightsId = table.Column<int>(type: "int", nullable: false),
+                    TripsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JourneyFlight", x => new { x.FlightsId, x.TripsId });
+                    table.ForeignKey(
+                        name: "FK_JourneyFlight_flight_FlightsId",
+                        column: x => x.FlightsId,
+                        principalTable: "flight",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_JourneyFlight_journey_TripsId",
+                        column: x => x.TripsId,
+                        principalTable: "journey",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_flight_fk_IdTrasport",
+                table: "flight",
+                column: "fk_IdTrasport");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JourneyFlight_TripsId",
+                table: "JourneyFlight",
+                column: "TripsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JourneyFlight");
+
+            migrationBuilder.DropTable(
+                name: "flight");
+
+            migrationBuilder.DropTable(
+                name: "journey");
+
+            migrationBuilder.DropTable(
+                name: "trasport");
+        }
+    }
+}
