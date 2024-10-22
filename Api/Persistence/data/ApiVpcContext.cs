fd8549e1471e12466364a0cf74191eef6690c588
williamvg93 <sonico_5071@hotmail.com>
2024-10-21 22:25:06 -0500

    feat: :sparkles: DB Context Added to the persistence project
    The configuration for the entities is created with the dbSet and a configuration is added to create the entity models in separate classes

---
+++ Api/Persistence/data/ApiVpcContext.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Domain.entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.data
+{
+    public class ApiVpcContext : DbContext
+    {
+        public ApiVpcContext(DbContextOptions<ApiVpcContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Transport> Transports { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Journey> Journeys{ get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
